68ef623447dd505cc40577349cf852371c068a32
PhongNL <tiphong113@gmail.com>
2025-04-22 18:04:33 +0700

    basic structure, stuck at shrink down to max size
    Signed-off-by: PhongNL <tiphong113@gmail.com>

--- image.cpp
+++ image.cpp
@@ -2,18 +2,35 @@
 #include <QImage>
 #include <QImageReader>
 #include <QImage>
-#include <QDialog>
+#include <QFileDialog>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QGroupBox>
 #include <QPushButton>
+#include <QLabel>
+#include <QScreen>
+#include <QPixmap>
 
 image::image(QWidget *parent)
     : QWidget(parent)
 {
     QHBoxLayout * mainLayout = new QHBoxLayout(this);
+
+    int screenWidth = QGuiApplication::primaryScreen()->availableGeometry().width();
+    int screenHeight = QGuiApplication::primaryScreen()->availableGeometry().height();
+    this->resize(screenWidth, screenHeight);
+
+    QGroupBox * imageBox =  new QGroupBox("Image");
+    QHBoxLayout * imageLayout = new QHBoxLayout(imageBox);
+    QLabel * imageLabel = new QLabel(imageBox);
+    imageLabel->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+    imageLabel->setAlignment(Qt::AlignCenter);
+    imageLayout->addWidget(imageLabel);
+    imageBox->setLayout(imageLayout);
+    mainLayout->addWidget(imageBox, 9);
+
     QVBoxLayout * rightPanel = new QVBoxLayout();
-    mainLayout->addLayout(rightPanel);
+    mainLayout->addLayout(rightPanel, 1);
 
     QGroupBox * inputBox = new QGroupBox("Input");
     QVBoxLayout * inputLayout = new QVBoxLayout(inputBox);
@@ -40,6 +57,35 @@ image::image(QWidget *parent)
     rightPanel->addWidget(inputBox, 1);
     rightPanel->addWidget(modifyBox, 8);
     rightPanel->addWidget(outputBox, 1);
+
+
+   // OPEN IMAGE
+    QString imageFileName;
+    connect(openButton, &QPushButton::clicked, this, [this, &imageFileName, imageLabel](){
+        imageFileName = QFileDialog::getOpenFileName(this, "Open Image", QDir::homePath(), "Images (*.png *.xpm *.jpg)");
+        //qInfo() << "Path: " << imageFileName;
+        if (imageFileName.isEmpty())
+            {
+            qInfo("No file selected");
+            return;
+        }
+        QImageReader imageReader(imageFileName);
+        if (!imageReader.canRead())
+        {
+            qWarning("Image file is invalid");
+            return;
+        }
+        QPixmap imagePixmap = QPixmap::fromImageReader(&imageReader);
+        qInfo() << "imagePixmap: " << imagePixmap.size();
+        qInfo() << "imageLabel: " << imageLabel->size();
+        if (imagePixmap.width() > imageLabel->width() || imagePixmap.height() > imageLabel->height())
+        {
+            imagePixmap.scaled(imageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+            qInfo() << "imagePixmap: " << imagePixmap.size();
+        }
+        imageLabel->setPixmap(imagePixmap);
+
+    });
 }
 
 image::~image()
